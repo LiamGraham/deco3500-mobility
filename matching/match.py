bfa14988a8c28f372ddd6c878a6394ce89d9d619
LiamGraham <liam.david.graham@gmail.com>
2020-09-25 15:22:15 +1000

    Add match nodes

--- matching/match.py
+++ matching/match.py
@@ -5,6 +5,8 @@ import pprint
 
 from attributes import Attribute, BinaryAttribute, MultiAttribute
 
+class MatchError(Exception):
+    pass
 
 class User:
 
@@ -46,6 +48,103 @@ class User:
 
     def __str__(self):
         return self.__repr__()
+
+
+class Matcher:
+
+    def __init__(self, users):
+        self.users = users
+        self.scores = {}
+        self._threshold = 0
+        self.match_all()
+
+    def match_all(self):
+        self.scores = {user:{} for user in users}
+        for i in range(len(users)):
+            user1 = users[i]
+            for j in range(len(users)):
+                if i == j:
+                    continue
+                user2 = users[j]
+                score = user1.compare(user2)
+                self.scores[user1][user2] = score
+        self._compute_threshold()
+
+    def _compute_threshold(self):
+        all_values = []
+        seen = set()
+        for user1, values in self.scores.items():
+            for user2, value in values.items():
+                if user2 in seen:
+                    continue
+                all_values.append(value)
+            seen.add(user1)
+        all_values.sort(reverse=True)
+        upper = all_values[:len(all_values) // 2]
+        self._threshold = upper[len(upper) // 2]
+
+    def get_matches(self, user):
+        if user not in self.scores:
+            return []
+        matches = []
+        scores = self.scores[user]
+        for candidate, score in scores.items():
+            if score > self._threshold:
+                matches.append(candidate)
+        return matches
+
+    def get_score(self, user1: User, user2: User) -> float:
+        """Returns match score for user1 and user2 if a score exists, otherwise 0.
+
+        Args:
+            user1 (User): First user 
+            user2 (User): Second user
+
+        Returns:
+            float: Match score for user1 and user2 if a score exists, otherwise 0
+        """
+        if user1 not in self.scores:
+            return 0
+        return self.scores[user1].get(user2, 0)
+
+
+class MatchNode:
+
+    def __init__(self, user: User, matches: List["MatchNode"]=None):
+        self.user = user
+        if matches:
+            self.matches = matches
+        else:
+            self.matches = []
+
+    def add_match(self, match: "MatchNode"):
+        if match not in self.matches:
+            self.matches.append(match)
+
+    def add_matches(self, matches: List["MatchNode"]):
+        for match in matches:
+            self.add_match(match)
+
+    def __repr__(self):
+        return f"MatchNode(user={self.user.name}, matches={[user.name for user in self.matches]})"
+
+    def __str__(self):
+        return self.__repr__()
+
+def generate_match_nodes(matcher):
+    nodes = {}
+    root = matcher.users[0]
+
+    for user in users:
+        if user not in nodes:
+            node = MatchNode(user)
+            nodes[user] = node
+        else:
+            node = nodes[user]
+
+        matches = matcher.get_matches(user)
+        node.add_matches(matches)
+    return nodes
 
 def generate_users(attr_path: str, size: int, seed: bool=None) -> List[User]:
     """Generates a list of random users of the given size. 
@@ -129,28 +228,6 @@ def compute_scores(users) -> Dict[str, Dict[str, float]]:
     return scores
 
 
-def compute_mean(scores: Dict[str, Dict[str, float]]) -> float:
-    """Returns the mean score of the given set of scores, ignoring duplicates.
-
-    Args:
-        scores (Dict[str, Dict[str, float]]): Scores for which mean will be calculated
-
-    Returns:
-        float: Mean score
-    """
-    seen = {}
-    total = count = 0
-    for name1, values in scores.items():
-        for name2, value in values:
-            if name2 in seen:
-                continue
-            total += value
-            count += 1
-        seen.add(name1)
-    mean = total / count
-    return mean
-
-
 def save_users(users, path):
     user_json = []
     for user in users:
@@ -168,11 +245,16 @@ def save_scores(scores, path):
 if __name__ == "__main__":
     printer = pprint.PrettyPrinter()
 
-    #users = generate_users("matching/categories.json", 10, seed=1)
-    users = load_users("matching/examples/users1.json")
-    scores = compute_scores(users)
-    printer.pprint(users)
-    printer.pprint(scores)
+    users = generate_users("matching/categories.json", 10, seed=1)
+    #users = load_users("matching/examples/users1.json")
 
-    #save_users(users, "matching/examples/users1.json")
-    #save_scores(scores, "matching/examples/scores1.json")
+    print("Computing match scores")    
+    matcher = Matcher(users)
+    #printer.pprint(users)
+    #printer.pprint(matcher.scores)
+
+    print("Generating match nodes")
+    nodes = generate_match_nodes(matcher)
+    printer.pprint(list(nodes.values()))
+    #save_users(users, "matching/examples/users2.json")
+    #save_scores(scores, "matching/examples/scores2.json")
